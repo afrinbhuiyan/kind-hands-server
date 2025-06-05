993b03c2e998916e7d0b0169d1e7153b25e8b5df
afrinbhuiyan <mstafrinbhuiyan@gmail.com>
2025-06-06 00:48:03 +0600

    connect mongoDB database and hide user and password

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+// const { MongoClient, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,10 +11,34 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cb9e028.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 // Root Route
-app.get('/', (req, res) => {
-  res.send('Volunteer Post Server is running');
+app.get("/", (req, res) => {
+  res.send("Volunteer Post Server is running");
 });
 
 // Start Server
